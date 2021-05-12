31330ee9608ed75d1a0677ce8661e12b7a4c5d73
jqsook@yahoo.com <you@example.com>
2021-05-12 14:11:59 -0600

    updt

---
+++ apps.js
@@ -0,0 +1,25 @@
+// Table of contents - Code flow order
+// 1. Play Function
+    //--playerChoice
+// 2.
+
+// The function should accept a single argument that represent the players choice: "rock" "paper" or "scissors".
+
+// ### Determine a Winner!
+
+// We are now going to add logic to the `play function` that will determine who wins the game.
+// This will require a you to use `if` `else` statements.
+
+// **TIP:** Before writing code, it may be beneficial to write down on a scrap of paper the logic that will be used
+// to determine a winner. Writing in plain english before writing in code is a good idea!!!
+
+
+
+function goButton() {
+    // Try and get this to display the 3 center cards -
+    // so the cards will need to start hidden- and the button will remove the hidden element
+}
+
+function play(playerChoice) {
+    let computerChoice = "rock"
+}
